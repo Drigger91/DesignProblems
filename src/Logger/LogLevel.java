22e17f3e89c0231047e0dfb9ebb88c3a42a2ce6c
Drigger91 <82764734+Drigger91@users.noreply.github.com>
2024-06-16 19:35:25 +0530

    some design coding problems

---
+++ src/Logger/LogLevel.java
@@ -0,0 +1,25 @@
+package Logger;
+
+public enum LogLevel{
+    DEBUG("debug", 1),
+    INFO("warn", 2),
+    WARN("info", 3),
+    ERROR("error", 4);
+
+    private final String name;
+    private final int priority;
+
+    LogLevel(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
+
---
+++ src/Logger/Logger.java
@@ -0,0 +1,8 @@
+package Logger;
+
+public interface Logger {
+    void debug(Object message);
+    void error(Object message);
+    void info(Object message);
+    void warn(Object message);
+}
